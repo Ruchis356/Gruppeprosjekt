e19ccf35ae262fb50dfddb0183fff8d4b0adb751
escaaaaaa <hannmha@stud.ntnu.no>
2025-03-12 16:58:08 +0100

    Updating visual table presentation to be scrollable

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-#A class to import and handle environmental data (weather and air quality)
+# Importing the display-funtion for a jupyter file in a conditional manner
+try:
+    from IPython.display import display, HTML
+    IN_JUPYTER = True
+except ImportError:
+    IN_JUPYTER = False
+
+# A class to import and handle environmental data (weather and air quality)
 class VisualTable:
 
-    #Initialize the EnvironmentalData class
+    # Initialize the EnvironmentalData class
     def __init__(self):
         self.df = None
 
@@ -16,4 +23,22 @@ class VisualTable:
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', None)
         pd.set_option('display.width', None)
-        display(df)
+
+        # If the function is called in a jupyter notebook, make the dataframe HTML and scrollable
+        if IN_JUPYTER:
+            df_html = df.to_html()
+
+            # Wrap the HTML table in a scrollable div
+            scrollable_table = f"""
+            <div style="overflow-x: auto; overflow-y: auto; max-height: 400px; max-width: 100%;">
+                {df_html}
+            </div>
+            """
+
+            # Display the scrollable table
+            display(HTML(scrollable_table))
+
+        # Outside of jupyter notebooks, the table is printed as normal if needed
+        else:
+            print(df)
+
