757b9102b632f542f73f19b1e5aedcffeb3fd1c4
escaaaaaa <hannmha@stud.ntnu.no>
2025-03-12 16:24:26 +0100

    Started writing unittests

---
+++ Test/test_missing_value.py
@@ -0,0 +1,7 @@
+import unittest
+import sys, os
+
+# Add the parent directory to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from data_handling import RefinedData
